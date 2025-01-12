a63048fb2cdc0164b632f814cdd3255c042105e1
buyixiao <2391527690@qq.com>
2025-01-12 21:51:39 +0800

    final bugfix

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -14,7 +14,7 @@ export const addPost = async (prevState, formData) => {
     // const desc = formData.get("desc")
     // const slug = formData.get("slug")
 
-    const { title, desc, slug, userId } = Object.fromEntries(formData)
+    const { title, desc, slug, userId, img } = Object.fromEntries(formData)
 
     console.log(title, desc, slug, userId)
 
@@ -25,7 +25,8 @@ export const addPost = async (prevState, formData) => {
             title,
             desc,
             slug,
-            userId
+            userId,
+            img
         })
 
         await newPost.save()
